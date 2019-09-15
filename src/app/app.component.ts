8f5aebe935e7a993607ade71b4f00df76013ff82
merrymaker14 <tmusharapov@bk.ru>
2019-09-15 12:21:54 +0300

    First steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent {
   ngOnInit() {
     this.myFirstService.getData(this.LOLAPIKEY).subscribe(data => {
       this.records = data;
+      console.log(this.records);
     })
   }
 }
